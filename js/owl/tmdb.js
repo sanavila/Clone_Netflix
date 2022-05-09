9918e53894b0e4a4e84149ea06cc34cd544ed3ca
sanavila <rute.leone@hotmail.com>
2022-05-08 23:02:07 -0300

    consumindo API, retirada owl carrousel

---
+++ js/owl/tmdb.js
@@ -0,0 +1,27 @@
+const API_KEY = '1d3066c4352f9064bb36708cbe10d9ae';
+const API_BASE = 'https://api.themoviedb.org/3';
+const rolMovies = document.querySelector('.rolMovies');
+const carrossel = document.querySelector('.carrossel-filmes');
+
+const mostrarImg = (url) => {   
+    rolMovies.innerHTML += `<img src="${url}" >`
+}
+
+const getMovies = async () => {
+    try {
+        const data = await fetch(API_BASE + `/movie/top_rated?language=pt-BR&api_key=${API_KEY}`);
+        const movies = await data.json(); 
+        movies.results.map((item) => mostrarImg(`https://image.tmdb.org/t/p/w300${item.poster_path}`));
+    } catch (e) {
+        return e
+    } 
+}
+
+getMovies()
+
+const retroceder = () => {
+    carrossel.scrollLeft -= 100;
+}
+const avancar = () => {
+    carrossel.scrollLeft += 100;
+}
